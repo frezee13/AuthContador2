d7ffe0ad5115ebffb17c393cba97a64c232564f3
frezee13 <frezee13@gmail.com>
2018-04-06 20:58:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pc_seven/authcontador/AuthContador.java
@@ -0,0 +1,107 @@
+package com.example.pc_seven.authcontador;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.auth.api.Auth;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.firebase.ui.auth.AuthUI;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AuthContador extends AppCompatActivity {
+
+
+    private Button btnIniciar;
+    private static final int RC_SIGN_IN = 123;
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth_contador);
+
+        btnIniciar = (Button) findViewById(R.id.btn_iniciar);
+
+        btnIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mAuth = FirebaseAuth.getInstance();
+                FirebaseUser usuarioActual = mAuth.getCurrentUser();
+
+                if (usuarioActual == null) {
+                    List<AuthUI.IdpConfig> providers = Arrays.asList(
+                            new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build());
+
+                    /* Create and launch sign-in intent */
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setAvailableProviders(providers)
+                                    .build(),
+                            RC_SIGN_IN);
+
+                } else {
+                    Intent mIntent = new Intent(AuthContador.this, session_Iniciada.class);
+                    startActivity(mIntent);
+                    finish();
+                }
+
+                }
+            });
+        }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                // Successfully signed in
+                Intent mIntent = new Intent(AuthContador.this, session_Iniciada.class);
+                startActivity (mIntent);
+                finish();
+                // ...
+            } else {
+                Toast.makeText(AuthContador.this,
+                        "La session no fue inicidad satisfactoriamente ", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+}
+
---
+++ app/src/main/java/com/example/pc_seven/authcontador/session_Iniciada.java
@@ -0,0 +1,16 @@
+package com.example.pc_seven.authcontador;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class session_Iniciada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_session__iniciada);
+
+
+
+    }
+}
